fd7dddb55ff34e76ce0fd27d5ba981cc35104631
Meiri Bernardina <51059393+MeiriBernardina@users.noreply.github.com>
2022-04-18 17:37:49 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,69 @@
+const fromText = document.querySelector(".from-text"),
+toText = document.querySelector(".to-text"),
+selectTag = document.querySelectorAll("select"),
+exchangeIcon = document.querySelector(".exchange"),
+translateBtn = document.querySelector("button"),
+icons = document.querySelectorAll(".row i");
+
+selectTag.forEach((tag, id) => {
+    for (let country_code in countries) {
+        let selected = id == 0 ? country_code == "en-GB" ? "selected" : "" : country_code == "hi-IN" ? "selected" : "";
+        let option = `<option ${selected} value="${country_code}">${countries[country_code]}</option>`;
+        tag.insertAdjacentHTML("beforeend", option);
+    }
+});
+
+
+exchangeIcon.addEventListener("click", () => {
+    // exchanging textarea and select tag values
+    let tempText = fromText.value,
+    tempLang = selectTag[0].value;
+    fromText.value = toText.value;
+    toText.value = tempText;
+    selectTag[0].value = selectTag[1].value;
+    selectTag[1].value = tempLang;
+});
+
+fromText.addEventListener("keyup", () => {
+    if(!fromText.value) {
+        toText.value = "";
+    }
+});
+
+translateBtn.addEventListener("click", () => {
+    let text = fromText.value,
+    translateFrom = selectTag[0].value, //getting fromSelect tag value
+    translateTo = selectTag[1].value; //getting toSelect tag value
+    if(!text) return;
+    toText.setAttribute("placeholder", "Tranlating..." );
+    let apiUrl = `https://api.mymemory.translated.net/get?q=${text}&langpair=${translateFrom}|${translateTo}`;
+    //fetching api response and returning it with parsing into js obj
+    // and in another then method receiving that obj
+    fetch(apiUrl).then(res => res.json()).then(data => {
+        toText.value = data.responseData.translatedText;
+        toText.setAttribute("placeholder", "Tranlation" );
+    });
+});
+
+icons.forEach(icon => {
+    icon.addEventListener("click", ({target}) => {
+        if(!fromText.value || !toText.value) return;
+        if(target.classList.contains("fa-copy")) {
+        //if clicked icon has from id, copy the fromTextarea value else copy the toTextarea value
+            target.id == "from" ? navigator.clipboard.writeText(fromText.value) :  navigator.clipboard.writeText(toText.value);
+        } else {
+            let utterance;
+        //if clicked icon has from id, copy the fromTextarea value else speak the toTextarea value
+            if(target.id == "from") {
+                utterance = new SpeechSynthesisUtterance(fromText.value)
+                utterance.lang = selectTag[0].value; //setting utterance languange to fromSelect tag value
+            } else {
+                utterance = new SpeechSynthesisUtterance(toText.value)
+                utterance.lang = selectTag[1].value; //setting utterance language to toSelect tag value
+            }
+            speechSynthesis.speak(utterance); // speak the passed utterance
+        }
+    });
+});
+
+
